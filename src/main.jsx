838b1f09bc2d987f1dad8261ba695e00dd3cf91a
stoirov407-lang <stoirov407@gmail.com>
2026-08-26 10:10:01 +0500

    Use standard React entrypoint for 3D app

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,8 @@
-import './landing.jsx'
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './landing.jsx'
 import './cityEnhancements.js'
 import './cityControls.css'
+
+const root = document.getElementById('root')
+createRoot(root).render(<App />)
