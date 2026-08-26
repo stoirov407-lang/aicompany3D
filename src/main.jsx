42bfc78daf9c410570b8ce9a2643e9ac332560cf
stoirov407-lang <stoirov407@gmail.com>
2026-08-26 17:02:31 +0500

    switch app entry to interactive office

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './landing.jsx'
+import App from './app2.jsx'
 import './cityEnhancements.js'
 import './cityControls.css'
 
-const root = document.getElementById('root')
-createRoot(root).render(<App />)
+createRoot(document.getElementById('root')).render(<App />)
